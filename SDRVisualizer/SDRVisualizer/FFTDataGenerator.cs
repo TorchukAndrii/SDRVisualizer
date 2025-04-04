2e8d3ca9ba9ff0ce673984d45900acca161f3f04
Andrii.Torchuk <Andrii.Torchuk2@gmail.com>
2025-04-04 17:40:33 +0300

    Improve FFTDataGenerator

--- SDRVisualizer/SDRVisualizer/FFTDataGenerator.cs
+++ SDRVisualizer/SDRVisualizer/FFTDataGenerator.cs
@@ -4,51 +4,62 @@ public class FFTDataGenerator
 {
     private readonly int[,] _data;
     private readonly Random _rand;
-    private double _phaseShift;
+    private readonly int cols;
+    private readonly int rows;
     private int rowIndex;
-    private int rows;
-    private int cols;
-
-    // Constructor that initializes the FFT data with default values.
+    
     public FFTDataGenerator(int rows = 200, int cols = 1024)
     {
         this.rows = rows;
         this.cols = cols;
         _data = new int[rows, cols];
         _rand = new Random();
-        _phaseShift = 0;
         rowIndex = 0;
         GenerateData();
     }
-    
+
     private void GenerateData()
     {
-        for (int r = 0; r < rows; r++)
+        var spikeCount = 3;
+        for (var r = 0; r < rows; r++)
         {
-            for (int c = 0; c < cols; c++)
-            {
-                _data[r, c] = _rand.Next(-65, -55); // Mostly steady data
-            }
-            
+            for (var c = 0; c < cols; c++) _data[r, c] = _rand.Next(-100, -90); // Mostly steady data
+
             // First 5 columns always max value
-            for (int c = 0; c < 5; c++)
-            {
-                _data[r, c] = -20;
-            }
-            
+            for (var c = 0; c < 5; c++) _data[r, c] = -20;
+
             // Last 5 columns always min value
-            for (int c = cols - 5; c < cols; c++)
-            {
-                _data[r, c] = -120;
-            }
-            
-            // Introduce small spikes in every row
-            int spikeCount = _rand.Next(3, 6); // Between 3 and 5 spikes per row
-            for (int i = 0; i < spikeCount; i++)
-            {
-                int spikeCol = _rand.Next(5, cols - 5); // Choose a random column for the spike, avoiding fixed min/max zones
-                _data[r, spikeCol] = _rand.Next(-80, -50); // Generate a smaller spike
-            }
+            for (var c = cols - 5; c < cols; c++) _data[r, c] = -120;
+
+            // Add small spikes
+            if (r % 20 == 0 && r != 0 && r % 100 != 0)
+                for (var i = 0; i < spikeCount; i++)
+                {
+                    var spikeCol = _rand.Next(15, cols - 15);
+                    for (var spikeRow = r; spikeRow > r - 10; spikeRow--)
+                    for (var j = 0; j < 5; j++)
+                        if (r % 40 == 0)
+                        {
+                            _data[spikeRow, spikeCol + j] = _rand.Next(-120, -100);
+                            _data[spikeRow, spikeCol - j] = _rand.Next(-120, -100);
+                        }
+                        else
+                        {
+                            _data[spikeRow, spikeCol + j] = _rand.Next(-40, -20);
+                            _data[spikeRow, spikeCol - j] = _rand.Next(-40, -20);
+                        }
+                }
+            else if (r != 0 && r % 100 == 0) // Add big spikes
+                for (var i = 0; i < spikeCount; i++)
+                {
+                    var spikeCol = _rand.Next(70, cols - 70);
+                    for (var spikeRow = r; spikeRow > r - 20; spikeRow--)
+                    for (var j = 0; j < 30; j++)
+                    {
+                        _data[spikeRow, spikeCol + j] = _rand.Next(-110 + j, -50 - j);
+                        _data[spikeRow, spikeCol - j] = _rand.Next(-110 + j, -50 - j);
+                    }
+                }
         }
     }
 
